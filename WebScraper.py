fa04b06c588afcf6578a6b0a611dfc4f419c64c8
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-01-23 17:43:06 -0600

    Appending Scraped Webpage Chapters
    The scraped webpage chapters are combined into a single text document. The data is them dumped into a pickle file. Also, we use the sleep function between chapters to prevent being detected by the target server as a bot or from triggering any automatic DDOS protection mechanism.

--- WebScraper.py
+++ WebScraper.py
@@ -12,3 +12,14 @@ for c in range(chapters):
     chapter_urls.append(url)
     #print(url)
 
+# Get URL HTML
+chapter_htmls = []
+for c in range(chapters):
+    html = requests.get(chapter_urls[c])
+    chapter_htmls.append(html.text)
+    time.sleep(1) # to prevent DDOS lock from websites detecting bots / IP traffic
+    #print(html.text)
+
+# Write HTML to pickle file
+with open("temp\html_list", "wb") as fp:
+    pickle.dump(chapter_htmls, fp)
