5bddb010f274360af217d57e82bffe7fa450db9f
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-01-25 21:42:30 -0600

    HTML Parser Verification
    Added a quick verification to check if the extracted chapters and titles match the expected number of chapters and index of the book. The scraped chapter text and titles are then saved as a pickle file.

--- HTMLparser.py
+++ HTMLparser.py
@@ -31,3 +31,18 @@ for c in range(chapters):
     chapter_text.append(story)
     #print(story)
 
+# Verify Chapter Titles match Chapters
+print("Titles : " + str(len(chapter_titles)))
+print("Chapters : " + str(len(chapter_text)))
+if len(chapter_titles) == len(chapter_text):
+    print("Match")
+    
+else:
+    print("Warning! Chapter Titles do not match Chapters")
+
+# Write Chapters to pickle file
+with open("temp\chapter_titles", "wb") as fp:
+    pickle.dump(chapter_titles, fp)
+
+with open("temp\chapter_text", "wb") as fp:
+    pickle.dump(chapter_text, fp)
