6ddf5c4156ddfddb36f8908073bdcc29730fe097
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-01-25 21:40:37 -0600

    Example: Extracting Chapters from HTML Book
    A script for automatically finding all <p> elements and the text within for extracting all the text from a book chapter. The scraped text is appended to form the whole story.

--- HTMLparser.py
+++ HTMLparser.py
@@ -17,3 +17,17 @@ for chapter_item in chap_title_page.find_all('a'):
     chapter_titles.append(chapter_item.get_text())
     #print(chapter_item.get_text())
 
+# Extract specific content from HTML
+chapters = len(chapter_htmls)
+for c in range(chapters):
+    #print(chapter_htmls[c])
+    html = BeautifulSoup(chapter_htmls[c], 'html.parser')
+    page = html.find(id="contentdata")
+    text = page.ul.contents[13]
+    story = ""
+    for paragraph in text.find_all('p'):
+        story += "\n\n"
+        story += paragraph.get_text()
+    chapter_text.append(story)
+    #print(story)
+
