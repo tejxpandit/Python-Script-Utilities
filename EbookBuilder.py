ab3d0694d3c4e4f2511ff73461048189f4901a72
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-01-26 23:08:57 -0600

    Create Ebook Object
    Create an ebook objects using the ebooklib library. Add metadata to the ebook and set the cover image. Also add a spline with the index.

--- EbookBuilder.py
+++ EbookBuilder.py
@@ -21,3 +21,17 @@ else:
 
 chapters = len(chapter_text)
 
+# Create Ebook
+book = epub.EpubBook()
+
+book.set_identifier('xxxxxx')
+book.set_title('Book Title')
+book.set_language('en')
+book.add_author('Author Name')
+book.add_metadata('DC', 'description', "The description of the story and what happens in the book to all the characters.")
+
+book.set_cover("image.jpg", open('temp\cover.jpg', 'rb').read())
+
+spine = ["cover", "nav"]
+toc = ()
+
