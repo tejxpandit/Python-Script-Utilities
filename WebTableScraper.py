b2ba4bb1b4b263c2ec11ea764716f55a2a562e6a
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-03-03 23:43:46 -0600

    Extract Table from HTML
    Parse the Table and its Rows from the HTML. Note that the requests library uses the "post" method instead of "get" because the target is a PHP document.

--- WebTableScraper.py
+++ WebTableScraper.py
@@ -7,3 +7,12 @@ URL = "https://www.nuailab.com/events/workshops/ai_winterschool2024/readtest.php
 
 html = requests.post(URL, headers={"User-Agent": "Tej Pandit"})
 
+#print(html.text)
+
+html_parser = BeautifulSoup(html.text, 'html.parser')
+
+table = html_parser.find("table")
+
+# Extract all rows
+rows = table.find_all("tr")
+
