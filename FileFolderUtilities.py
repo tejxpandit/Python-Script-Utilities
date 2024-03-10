4c5d408c77c0889e873f62bc30327d767e95bcdb
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-03-09 21:07:21 -0600

    Folder Deletion using shutil
    Using the shutil library to delete target folders in python. Inserted into a try/except statement to handle errors.

--- FileFolderUtilities.py
+++ FileFolderUtilities.py
@@ -3,6 +3,9 @@ import shutil
 # Target Folder Path
 folder_path = "/path/folder"
 
+# Delete Folder
 try:
   shutil.rmtree(folder_path)
   print(f"Folder '{folder_path}' and its contents have been deleted.")
+except OSError as e:
+  print(f"Error: {e}")
